0a2c0e26f1cd56d9a22456972bc9a04cef4c50c8
Jinai <7669687+Jinai@users.noreply.github.com>
2016-12-07 01:52:00 +0100

    Updated build script

--- src/setup.py
+++ src/setup.py
@@ -22,22 +22,22 @@ def py2exe():
                             "bundle_files": 2,
                             "dist_dir": dist_dir,
                             "dll_excludes": ['msvcr71.dll', 'pywintypes34.dll'],
-                            "excludes": ['doctest', 'unittest', 'xml', 'xmlrpc', 'difflib',
-                                         'optparse', 'dis', 'bz2', 'bdb', 'ftplib', 'optparse', 'pdb', 'pydoc',
-                                         'pyexpat', 'pywintypes', 'selectors', 'socketserver', 'win32api', 'win32con',
-                                         '_bz2', '_hashlib', '_lzma', '_ssl', 'argparse', 'gettext', 'netbios', 'netrc',
-                                         'pkgutil', 'plistlib', 'pprint', 'py_compile', 'runpy', 'ssl', 'textwrap',
-                                         'win32wnet', 'zipfile', '_multiprocessing', '_osx_support',
-                                         '_strptime', '_threading_local', 'lzma', 'gzip', 'glob', 'getopt', 'getpass',
-                                         'hmac', 'webbrowser', 'urllib', 'threading', 'subprocess', 'operators',
-                                         ],
-                            }
-                 },
+                            "excludes": ['doctest', 'unittest', 'xml', 'xmlrpc', 'difflib', 'optparse', 'dis', 'bz2',
+                                         'bdb', 'ftplib', 'optparse', 'pdb', 'pydoc', 'pyexpat', 'pywintypes',
+                                         'selectors', 'socketserver', 'win32api', 'win32con', '_bz2', '_hashlib',
+                                         '_lzma', '_ssl', 'netbios', 'netrc', 'pkgutil', 'plistlib', 'pprint',
+                                         'py_compile', 'runpy', 'ssl', 'win32wnet', 'zipfile', '_multiprocessing',
+                                         '_osx_support', '_strptime', '_threading_local', 'lzma', 'gzip', 'getopt',
+                                         'getpass', 'hmac', 'webbrowser', 'urllib', 'operators',
+                            ],
+                }
+        },
         zipfile=None,
         windows=[{"script": "RespoTool.py",
                   "icon_resources": [(0, "resources/respotool.ico")],
                  }],
-        data_files=[('resources', ['resources/respotool.ico'])]
+        data_files=[('resources', ['resources/respotool.ico', 'resources/tags.json', 'resources/respomaps.json',
+                                   'resources/archives_templates.json', 'resources/search.gif'])]
     )
 
 
