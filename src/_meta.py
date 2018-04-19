f8228fbdc0adbe108240d406e6c4bb11e2e8412d
Jinai <7669687+Jinai@users.noreply.github.com>
2018-04-19 02:24:35 +0200

    Version bump

--- src/_meta.py
+++ src/_meta.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # !python3
 
-__version__ = "1.5"
-__date__ = "08/02/2017"
+__version__ = "1.6"
+__date__ = "19/04/2018"
 __author__ = "Jinai"
 __email__ = "jinai.extinction@gmail.com"
