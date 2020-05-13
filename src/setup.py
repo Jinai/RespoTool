9bf4d754acbbc9ef7207c7a8cfa93f45620b8569
Jinai <jinai.git@gmail.com>
2020-05-13 03:16:31 +0200

    Update game URL in setup.py

--- src/setup.py
+++ src/setup.py
@@ -15,7 +15,7 @@ def py2exe():
         description="Outil de gestion de signalements de cartes pour le jeu Aaaah !",
         author="Jinai",
         author_email="jinai.git@gmail.com",
-        url="http://www.extinction.fr/minijeux/",
+        url="http://www.extinction-minijeux.fr/",
         options={
             "py2exe": {
                 "compressed": True,
