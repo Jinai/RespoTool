819fc2cb0c8b385db1638d6b56bdbd6c36d23558
Jinai <jinai.git@gmail.com>
2020-05-14 01:07:13 +0200

    Fix build scripts and add PyInstaller as a dependency

--- src/RespoTool.spec
+++ src/RespoTool.spec
@@ -2,11 +2,22 @@
 
 block_cipher = None
 
+added_files = [
+    ("data/contact.json", "data"),
+    ("data/duplicates_msg.json", "data"),
+    ("data/respomaps.json", "data"),
+    ("data/statuses.json", "data"),
+    ("data/tags.json", "data"),
+    ("data/img/respotool.ico", "data/img"),
+    ("data/img/search1.png", "data/img"),
+    ("data/img/search2.png", "data/img"),
+    ("data/img/shield_respo.png", "data/img")
+]
 
 a = Analysis(
     ["RespoTool.py"],
     binaries=[],
-    datas=[],
+    datas=added_files,
     hiddenimports=[],
     hookspath=[],
     runtime_hooks=[],
@@ -17,7 +28,7 @@ a = Analysis(
         "_lzma", "_ssl", "netbios", "netrc", "pkgutil", "plistlib", "pprint",
         "py_compile", "runpy", "ssl", "win32wnet", "zipfile", "_multiprocessing",
         "_osx_support", "_strptime", "_threading_local", "lzma", "gzip", "getopt",
-        "getpass", "hmac", "urllib", "operators",
+        "getpass", "hmac", "urllib", "operators"
     ],
     win_no_prefer_redirects=False,
     win_private_assemblies=False,
--- src/setup.py
+++ src/setup.py
@@ -3,18 +3,19 @@
 
 
 def py2exe():
-    from _meta import __version__
+    from _meta import __appname__, __author__, __email__, __version__
     from distutils.core import setup
+    # noinspection PyUnresolvedReferences
     import py2exe
 
     dist_dir = "../bin"
 
     setup(
-        name="RespoTool",
+        name=__appname__,
         version=__version__,
         description="Outil de gestion de signalements de cartes pour le jeu Aaaah !",
-        author="Jinai",
-        author_email="jinai.git@gmail.com",
+        author=__author__,
+        author_email=__email__,
         url="http://www.extinction-minijeux.fr/",
         options={
             "py2exe": {
@@ -30,7 +31,7 @@ def py2exe():
                     "_lzma", "_ssl", "netbios", "netrc", "pkgutil", "plistlib", "pprint",
                     "py_compile", "runpy", "ssl", "win32wnet", "zipfile", "_multiprocessing",
                     "_osx_support", "_strptime", "_threading_local", "lzma", "gzip", "getopt",
-                    "getpass", "hmac", "urllib", "operators",
+                    "getpass", "hmac", "urllib", "operators"
                 ],
             }
         },
@@ -38,13 +39,28 @@ def py2exe():
         windows=[
             {
                 "script": "RespoTool.py",
-                "icon_resources": [(0, "data/img/respotool.ico")],
+                "icon_resources": [(0, "data/img/respotool.ico")]
             }
         ],
         data_files=[
-            ("data", ["data/duplicates_msg.json", "data/respomaps.json", "data/tags.json", "data/statuses.json",
-                      "data/contact.json"]),
-            ("data/img", ["data/img/respotool.ico", "data/img/search.gif", "data/img/shield_respo.png"])]
+            (
+                "data", [
+                    "data/contact.json",
+                    "data/duplicates_msg.json",
+                    "data/respomaps.json",
+                    "data/statuses.json",
+                    "data/tags.json"
+                ]
+            ),
+            (
+                "data/img", [
+                    "data/img/respotool.ico",
+                    "data/img/search1.png",
+                    "data/img/search2.png",
+                    "data/img/shield_respo.png"
+                ]
+            )
+        ]
     )
 
 
