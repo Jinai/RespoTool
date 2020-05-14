9d20515b49d1cb6b39f551cc7ba62cd54fc328da
Jinai <jinai.git@gmail.com>
2020-05-14 21:50:18 +0200

    Bump version to 1.8 and update changelog

--- src/_meta.py
+++ src/_meta.py
@@ -2,7 +2,7 @@
 # !python3
 
 __appname__ = "RespoTool"
-__version__ = "1.7"
-__date__ = "08/02/2019"
+__version__ = "1.8"
+__date__ = "14/05/2020"
 __author__ = "Jinai"
 __email__ = "jinai.git@gmail.com"
